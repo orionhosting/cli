3720fa04e436f6a6088c960e1db8e3a870614299
cbrra <98268014+Cbrra@users.noreply.github.com>
2026-03-30 23:53:21 +0200

    feat: open command

---
+++ src/commands/open.ts
@@ -0,0 +1,40 @@
+import { platform } from "os";
+import chalk from "chalk";
+import figureSet from "figures";
+import openURL from "open";
+import ora from "ora";
+import { Context } from "../context";
+import { PANEL_URL } from "../utils/constants";
+
+/**
+ * The `open` command.
+ */
+export async function open(ctx: Context) {
+    await ctx.auth();
+    const project = await ctx.project();
+    const client = ctx.getPelicanClient();
+
+    ctx.printBanner();
+
+    const spinner = ora("Fetching server...\n").start();
+
+    let server;
+    try {
+        server = await client.servers.get(project.data.serverId);
+    } catch (err) {
+        spinner.stop();
+        ctx.handleException(err);
+        return;
+    }
+
+    spinner.color = "green";
+    spinner.text = "Opening panel...";
+
+    const url = `${PANEL_URL}/server/${server.attributes.identifier}`;
+    await openURL(url, { wait: platform() === "win32" });
+
+    spinner.stop();
+
+    console.log(chalk.bold.green(`  ${figureSet.tick} Panel opened in your browser.`));
+    console.log(`  Otherwise, go to ${chalk.gray(url)}`);
+}
