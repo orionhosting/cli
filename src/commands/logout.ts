6bad6cbec5bbf2463aff08755778ec72e72c4443
cbrra <98268014+Cbrra@users.noreply.github.com>
2026-03-30 23:53:11 +0200

    feat: logout command

---
+++ src/commands/logout.ts
@@ -0,0 +1,20 @@
+import chalk from "chalk";
+import figureSet from "figures";
+import { Context } from "../context";
+import { loadOrCreateAuth, saveAuth } from "../utils/auth";
+
+export async function logout(ctx: Context) {
+    ctx.printBanner();
+
+    const auth = await loadOrCreateAuth();
+
+    if (!auth.token) {
+        console.log(chalk.gray("  You are not logged in."));
+        return;
+    }
+
+    auth.token = "";
+    await saveAuth(auth);
+
+    console.log(chalk.gray(`${figureSet.tick} Logged out`));
+}
