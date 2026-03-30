74144b75f1b58805533a372043d9410cbb50baf8
cbrra <98268014+Cbrra@users.noreply.github.com>
2026-03-30 23:53:05 +0200

    feat: account command

---
+++ src/commands/account.ts
@@ -0,0 +1,43 @@
+import chalk from "chalk";
+import figureSet from "figures";
+import ora from "ora";
+import { Context } from "../context";
+
+/**
+ * The `account` command.
+ */
+export async function account(ctx: Context) {
+    await ctx.auth();
+    const client = ctx.getPelicanClient();
+
+    ctx.printBanner();
+
+    const spinner = ora("Loading account...\n").start();
+
+    let account;
+    try {
+        account = await client.account.get();
+    } catch (err) {
+        spinner.stop();
+        ctx.handleException(err);
+        return;
+    }
+
+    spinner.color = "yellow";
+    spinner.text = "Loading servers...\n";
+
+    let servers;
+    try {
+        servers = await client.servers.list();
+    } catch (err) {
+        spinner.stop();
+        ctx.handleException(err);
+        return;
+    }
+
+    spinner.stop();
+
+    console.log(`  ${chalk.gray("Logged in as")} ${chalk.bold.magenta(account.attributes.username)}`);
+    console.log(chalk.gray(`  ${figureSet.triangleRightSmall} email: ${account.attributes.email}`));
+    console.log(chalk.gray(`  ${figureSet.triangleRightSmall} servers: ${servers.meta.pagination.total}`));
+}
