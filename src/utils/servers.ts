f30c0eea3bf698daf14578dbbbf70b829fba70ad
cbrra <98268014+Cbrra@users.noreply.github.com>
2026-04-03 00:12:00 +0200

    feat: more server utils

--- src/utils/servers.ts
+++ src/utils/servers.ts
@@ -1,3 +1,5 @@
+import { ClientServer } from "@voctal/pelican";
+
 export const formatUptime = (value: number) => {
     // @ts-expect-error Intl.DurationFormat is missing from tslib, but is available as of Node v23
     const intl = new Intl.DurationFormat("en", { style: "narrow" });
@@ -8,4 +10,12 @@ export const formatUptime = (value: number) => {
         days: Math.floor(value / (1000 * 60 * 60 * 24)),
     });
     return uptime;
+};
+
+export const getServerNetworkData = (server: ClientServer) => {
+    const alloc = server.attributes.relationships?.allocations?.data[0];
+    const ip_alias = alloc?.attributes.ip_alias;
+    const port = alloc?.attributes.port;
+
+    return { allocation: alloc, ip_alias, port };
 };
