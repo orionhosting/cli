d153e3d13b12c7e663414b35737263ec57235793
cbrra <98268014+Cbrra@users.noreply.github.com>
2026-04-12 03:27:44 +0200

    feat: archive paths

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,3 +1,4 @@
+import { mkdir } from "node:fs/promises";
 import path from "node:path";
 import envPaths from "env-paths";
 
@@ -9,6 +10,10 @@ export const GLOBAL_CONFIG_DIR = paths.config;
 export const GLOBAL_AUTH_FILE = path.join(paths.config, "auth.json");
 export const GLOBAL_CONFIG_FILE = path.join(paths.config, "config.json");
 
+// Temporary paths
+
+export const TEMP_ARCHIVES_DIR = paths.temp;
+
 // Project paths (generated config)
 
 export const PROJECT_DIR_NAME = ".orion";
@@ -19,3 +24,12 @@ export const PROJECT_CONFIG_FILE = path.join(PROJECT_DIR, "project.json");
 
 export const PROJECT_USER_CONFIG_FILE_NAME = "orion.config.json";
 export const PROJECT_USER_CONFIG_FILE = path.join(process.cwd(), PROJECT_USER_CONFIG_FILE_NAME);
+
+// Utils
+
+/**
+ * Ensure the directory parent of `filepath` exists.
+ */
+export const makeDirectory = async (filepath: string) => {
+    await mkdir(path.dirname(filepath), { recursive: true });
+};
